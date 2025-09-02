33483608d101fc9f1ff695f664c5d2cb04d82d32
sheffykhlg <56907336+sheffykhlg@users.noreply.github.com>
2025-09-02 12:20:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,3 +14,9 @@ try:
     ADMIN_ID = int(os.getenv("ADMIN_USER_ID"))
 except (ValueError, TypeError):
     raise ValueError("Error: ADMIN_USER_ID environment variable aek valid integer nahi hai!")
+
+# MongoDB URI ko get karna
+MONGO_URI = os.getenv("MONGO_DB_URI")
+if not MONGO_URI:
+    raise ValueError("Error: MONGO_DB_URI environment variable set nahi hai!")
+
